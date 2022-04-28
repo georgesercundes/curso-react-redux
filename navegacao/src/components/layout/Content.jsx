be279eb6687515a969a044f6def0c1e10bde64ed
george.siqueira <gss@ecomp.poli.br>
2022-04-28 12:15:59 -0300

    Remoção da apostila

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,8 @@
+import "./Content.css";
+import React from "react";
+
+const Content = (props) => (
+  <aside className="Content">Componente Content</aside>
+);
+
+export default Content;
---
+++ navegacao/src/views/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+import Menu from "../components/layout/Menu";
+
+const App = (props) => (
+  <div className="App">
+    <Menu />
+  </div>
+);
+
+export default App;
